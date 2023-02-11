c273dc968338d06826b1497ee7d76b554e9661a9
Huang- <38799400+abusemydnac@users.noreply.github.com>
2023-02-11 15:24:06 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo " Docker......."
+
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+
+echo " container......."
+# Clone this repo
+git clone https://github.com/ObolNetwork/charon-distributed-validator-node.git
+
+# Change directory
+cd charon-distributed-validator-node
+
+# Create your charon ENR private key, this will create a charon-enr-private-key file in the .charon directory
+docker run --rm -v "$(pwd):/opt/charon" obolnetwork/charon:v0.13.0 create enr
