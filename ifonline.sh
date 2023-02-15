32b41de4e46aad707dfa3477c70f154c725f0b79
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-15 17:54:41 +0800

    Update ifonline.sh

--- ifonline.sh
+++ ifonline.sh
@@ -3,7 +3,7 @@ echo " Docker......."
 apt-get update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o -i /etc/apt/keyrings/docker.gpg 
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
