c925982b20d32cc37102ee405cba2d37878c1de8
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-15 13:52:42 +0800

    Update ifonline.sh

--- ifonline.sh
+++ ifonline.sh
@@ -9,7 +9,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-
+apt-get update -y
 apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt-get install php -y
 docker pull ghcr.io/iron-fish/ironfish:latest
