ced0a4cfd23dc39c031eebfeab337ccc186cc4c1
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-18 11:49:56 +0800

    Update oneupdate.sh

--- oneupdate.sh
+++ oneupdate.sh
@@ -1,2 +1,7 @@
-nohup php /root/phpcmd/MultiIFnodes.php 1 70> create.log 2>&1 &
-tail -f create.log
+# 12G+ 
+mkdir /root/phpcmd
+cd /root/phpcmd
+wget -O /root/phpcmd/MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
+wget -O /root/phpcmd/config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
+nohup php /root/phpcmd/MultiIFnodes.php 1 70 >/root/create.log 2>&1 &
+tail -f /root/create.log
