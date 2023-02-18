7783661da4ecedbee33e8772ec1d15f32cdb1e98
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-18 21:33:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ echo " Docker......."
 apt-get update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg
+curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
