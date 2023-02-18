7fea7d18a5240da90e8062b316e2255306ec439c
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-19 00:19:59 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -6,4 +6,5 @@ mkdir /root/phpcmd
 cd /root/phpcmd
 wget -O MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
 wget -O config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
-nohup php /root/phpcmd/MultiIFnodes.php 1 40  > /root/autorun.log 2>&1 &
+#nohup php /root/phpcmd/MultiIFnodes.php 1 40  > /root/autorun.log 2>&1 &
+php /root/phpcmd/MultiIFnodes.php 1 40 2>&1 | tee -a /root/autorun.log 2>/dev/null >/dev/null &
