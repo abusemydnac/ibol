08e6caaee38771d572948d071b6b4009d8f8a26f
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-18 21:49:49 +0800

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,6 @@
+#检查任务情况
+mkdir /root/phpcmd
+cd /root/phpcmd
+wget -O HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
+nohup php /root/phpcmd/HostingPointsCheck.php 1 120  > check.log 2>&1 &
+tail -f  check.log
