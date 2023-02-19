b36e06126887c4a428482e357349c3875c84585f
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-19 08:28:39 +0800

    Update MultiIFnodes.php

--- MultiIFnodes.php
+++ MultiIFnodes.php
@@ -12,40 +12,10 @@ if (count($argv) >= 3) {
 } else
     $maxNum = 1;
 $weeklink_grattifi = 'http://43.154.249.28:8000/index.php?r=ironfishacc/getgraffiti&task=newminer';
+$newstart = 0;
+
 for ($i = $minNum; $i <= $maxNum; $i++) {
     echo date("Y-m-d H:i:s") . "|$i/$maxNum| 获取需要部署的node.......\r\n";
-    $grattifi = file_get_contents($weeklink_grattifi);
-    $grattifi = trim($grattifi);
-    if (strlen($grattifi) > 20) {
-        echo "有点异常...\r\n";
-        echo $grattifi;
-        sleep(60);
-         $i--;
-        continue;
-    }
-    if (empty($grattifi)) {
-        echo "系统待执行任务为空....等待60秒...\r\n";
-        sleep(60);
-         $i--;
-        continue;
-    }
-    $isFinishHosting = getHostingPoints($grattifi);
-    if ($isFinishHosting) {
-        sleep(3);
-        $i--;
-        continue;
-    }
-    changeNodeGraffiti($grattifi,  $i);
-}
-
-
-function changeNodeGraffiti($grattifi,  $i)
-{
-    echo "获取到需要执行" . $grattifi . ".......\r\n";
-    $data = shell_exec("mkdir  /root/.node$i");
-    echo $data;
-    $data = shell_exec("chmod -R 777 /root/.node$i");
-    echo $data;
     $graffiti_file = "/root/.node$i/graffiti";
     if (is_file($graffiti_file)) {
         echo "\r\n" . $graffiti_file . "已存在默认阶段查看hosting时长.......\r\n";
@@ -59,9 +29,46 @@ function changeNodeGraffiti($grattifi,  $i)
             echo $data;
             $data = shell_exec("docker exec  node$i bash -c 'ironfish status'");
             echo $data;
-            return '';
+            sleep(3);
+            continue;
         }
+        
     }
+    $grattifi = file_get_contents($weeklink_grattifi);
+    $grattifi = trim($grattifi);
+    if (strlen($grattifi) > 20) {
+        echo "有点异常...\r\n";
+        echo $grattifi;
+        sleep(60);
+        $i--;
+        continue;
+    }
+    if (empty($grattifi)) {
+        echo "系统待执行任务为空....等待60秒...\r\n";
+        sleep(60);
+        $i--;
+        continue;
+    }
+    $isFinishHosting = getHostingPoints($grattifi);
+    if ($isFinishHosting) {
+        echo $grattifi . " 已完成! \r\n";
+        sleep(3);
+    }
+    //创建新任务
+    changeNodeGraffiti($grattifi,  $i);
+    sleep(3);
+}
+
+
+function changeNodeGraffiti($grattifi,  $i)
+{
+    echo "获取到需要执行" . $grattifi . ".......\r\n";
+    $data = shell_exec("mkdir  /root/.node$i");
+    echo $data;
+    $data = shell_exec("chmod -R 777 /root/.node$i");
+    echo $data;
+    $graffiti_file = "/root/.node$i/graffiti";
+
 
     $data = shell_exec("docker run -itd --name node$i --restart=always  --volume /root/.node$i:/root/.ironfish ghcr.io/iron-fish/ironfish:latest start");
     echo $data;
@@ -73,9 +80,9 @@ function changeNodeGraffiti($grattifi,  $i)
     echo $data;
     sleep(1);
     $data = shell_exec("docker restart node$i");
-    
+
     sleep(1);
-    
+
     $data = shell_exec("docker exec  node$i bash -c 'ironfish status'");
     echo $data;
     file_put_contents($graffiti_file, $grattifi);
@@ -92,7 +99,7 @@ function getHostingPoints($grattifi)
     //var_dump($data);
     echo "\r\n" . $grattifi . "|points:" . $data->points . "|websiteid:" . $websiteid . " |HostingPoints:" . $data->metrics->node_uptime->points .  " \r\n";
     if ($data->metrics->node_uptime->points >= 10)
-        file_get_contents('http://43.154.249.28:8000/index.php?r=ironfishacc/getwebsiteid&hostpoints='.$data->metrics->node_uptime->points.'&graffiti=' . $grattifi);
+        file_get_contents('http://43.154.249.28:8000/index.php?r=ironfishacc/getwebsiteid&hostpoints=' . $data->metrics->node_uptime->points . '&graffiti=' . $grattifi);
     if ($data->metrics->node_uptime->points >= 140) {
         return true;
     } else
