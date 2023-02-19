2e148a69242e6816012afe2c47c3285992ef65c3
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-19 15:12:50 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -1,9 +1,11 @@
 #重新开启线程
 echo " Docker......."
+
 apt-get update -y
 apt-get install ca-certificates curl gnupg lsb-release -y
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --batch --yes  --dearmor -io  /etc/apt/keyrings/docker.gpg 
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -12,6 +14,7 @@ apt-get install  docker-ce=5:20.10.23~3-0~ubuntu-focal -y
 # apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt-get install php -y
 apt install php7.4-cli -y
+
 rm -rf /root/.node*/*
 killall php
 docker rm $(docker ps -a | awk '{ print $1}' | tail -n +2) --force
