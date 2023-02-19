3ecc51c09a3472eda6b768fc667f01282024f142
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-19 10:47:12 +0800

    Update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -2,12 +2,13 @@
 docker pull ghcr.io/iron-fish/ironfish:latest
 #rm -rf /root/.node*/*
 docker rm $(docker ps -a | awk '{ print $1}' | tail -n +2) --force
-chmod -R 777 /phpcmd/config.json
-chmod -R 777 /root/.node*
+
 cd /root/
 mkdir /root/phpcmd
 cd /root/phpcmd
 wget -O MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
 wget -O config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
+chmod -R 777 /phpcmd/config.json
+chmod -R 777 /root/.node*
 #nohup php /root/phpcmd/MultiIFnodes.php 1 40  > /root/autorun.log 2>&1 &
 php /root/phpcmd/MultiIFnodes.php 1 40 2>&1 | tee -a /root/autorun.log 2>/dev/null >/dev/null &
