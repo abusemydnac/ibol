62f3a1686759ddeff118352e78fe31834df1065c
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-20 20:31:25 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,5 +2,6 @@
 mkdir /root/phpcmd
 cd /root/phpcmd
 wget -O HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
-nohup php /root/phpcmd/HostingPointsCheck.php 1 99  > check.log 2>&1 &
+dockernum=$(docker ps |grep node|wc -l)
+nohup php /root/phpcmd/HostingPointsCheck.php 1 dockernum  > check.log 2>&1 &
 tail -f  check.log
