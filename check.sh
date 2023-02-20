e2053757017f95c68f61c13cc4727ae25ba1456c
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-20 20:29:25 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,5 +2,5 @@
 mkdir /root/phpcmd
 cd /root/phpcmd
 wget -O HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
-nohup php /root/phpcmd/HostingPointsCheck.php 1 70  > check.log 2>&1 &
+nohup php /root/phpcmd/HostingPointsCheck.php 1 99  > check.log 2>&1 &
 tail -f  check.log
