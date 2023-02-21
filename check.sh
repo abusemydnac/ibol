34c715e7c7a678a30242a061bf8a8aec66b9008f
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-21 08:54:15 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #检查任务情况
 mkdir /root/phpcmd
 cd /root/phpcmd
-wget -O HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
-nohup php /root/phpcmd/HostingPointsCheck.php 1 $(docker ps |grep node|wc -l)  > check.log 2>&1 &
+wget -O MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
+nohup php /root/phpcmd/MultiIFnodes.php 1 $(docker ps |grep node|wc -l)  > check.log 2>&1 &
 tail -f  check.log
