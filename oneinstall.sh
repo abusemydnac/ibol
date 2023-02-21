65cc69e4e5fa4d3aed374d0f34c1b1d032946e6f
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-21 09:30:52 +0800

    Update oneinstall.sh

--- oneinstall.sh
+++ oneinstall.sh
@@ -2,4 +2,6 @@
 wget -O /root/ifonline.sh https://raw.githubusercontent.com/abusemydnac/ibol/main/ifonline.sh
 bash /root/ifonline.sh
 nohup php /root/phpcmd/MultiIFnodes.php 1 55 >/root/create.log 2>&1 &
+sleep 15
+echo 'DONE'
 # tail -f /root/create.log
