90e13c524bea0da06d525c42b4cd22cfbff9bb54
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-21 13:36:44 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,7 @@
-#检查任务情况
-mkdir /root/phpcmd
-cd /root/phpcmd
+# 12G+ 
 wget -O MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
-nohup php /root/phpcmd/MultiIFnodes.php 1 $(docker ps |grep node|wc -l)  > check.log 2>&1 &
-tail -f  check.log
+wget -O config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
+nohup php /root/phpcmd/MultiIFnodes.php 1 55 >/root/create.log 2>&1 &
+sleep 1
+echo 'DONE'
+# tail -f /root/create.log
