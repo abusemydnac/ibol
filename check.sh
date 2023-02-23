1f1bfecd4f3ac887d0e01be8b049a99c05ed04a6
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-24 07:12:05 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 # 12G+ 
-wget -O MultiIFnodes.php https://raw.githubusercontent.com/abusemydnac/ibol/main/MultiIFnodes.php
-wget -O config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
-nohup php /root/phpcmd/MultiIFnodes.php 1 55 >/root/create.log 2>&1 &
+wget -O /root/HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
+wget -O /root/config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
+nohup php /root/phpcmd/HostingPointsCheck.php 1 150 >/root/create.log 2>&1 &
 sleep 1
 echo 'DONE'
 # tail -f /root/create.log
