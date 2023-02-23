e27c16eb3b2cdef1e1fe206cc4577d0cbaabaa06
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-24 07:12:54 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 # 12G+ 
-wget -O /root/HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
-wget -O /root/config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
-nohup php /root/phpcmd/HostingPointsCheck.php 1 150 >/root/create.log 2>&1 &
+wget -O /root/phpcmd/HostingPointsCheck.php https://raw.githubusercontent.com/abusemydnac/ibol/main/HostingPointsCheck.php
+wget -O /root/phpcmd/config.json https://raw.githubusercontent.com/abusemydnac/ibol/main/config.json
+nohup php /root/phpcmd/HostingPointsCheck.php 1 150 >/root/check.log 2>&1 &
 sleep 1
 echo 'DONE'
-# tail -f /root/create.log
+# tail -f /root/check.log
