e032890d809048166301a3a982910a6628dd7c93
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-02-28 01:37:29 +0800

    Update init_obol.sh

--- init_obol.sh
+++ init_obol.sh
@@ -13,3 +13,7 @@ apt-get install  docker-ce=5:20.10.23~3-0~ubuntu-focal -y
 # apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt-get install php -y
 apt install php7.4-cli -y
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
