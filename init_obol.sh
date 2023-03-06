28e5cee366cdf099deb202145dc1bbc6a2545136
Gcaitou <38799400+abusemydnac@users.noreply.github.com>
2023-03-06 15:57:43 +0800

    Update init_obol.sh

--- init_obol.sh
+++ init_obol.sh
@@ -9,8 +9,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update -y
-apt-get install  docker-ce=5:20.10.23~3-0~ubuntu-focal -y
-# apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+# apt-get install  docker-ce=5:20.10.23~3-0~ubuntu-focal -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt-get install php -y
 apt install php7.4-cli -y
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
